eeaf1d2d0f12f186c585ea161215a5a402ddcb3d
KendallCC <117054402+KendallCC@users.noreply.github.com>
2023-02-07 14:20:29 -0600

    proyecto

--- Scripts/scripts.js
+++ Scripts/scripts.js
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
 _toggle.onclick = () => {
     _items.classList.toggle("open");
     _toggle.classList.toggle("close");
-=======
-_toggle.onclick = () => {
-    _items.classList.toggle("open");
-    _toggle.classList.toggle("close");
->>>>>>> 632bf03b59f2dfc44c35e9b1ebc286ab69101d1a
 }
